59e0bfc480a547c9efdf9641c48c7be8b44c7202
Patrick Mohler <pdmohlergm@gmail.com>
2022-11-15 15:52:12 -0500

    Fixed linting

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-param-reassign */
 /* eslint-disable max-len */
 
 /*
@@ -13,9 +12,9 @@
 */
 
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin); // The maximum is inclusive and the minimum is inclusive
 }
 
 function injectHTML(list) {
